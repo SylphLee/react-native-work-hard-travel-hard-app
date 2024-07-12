cbd84e33ab3c5ff2359ad595cf7cb5653c85bcc3
SylphLee <sylph0514@gmail.com>
2024-07-12 20:53:28 +0900

    #3 [2021 UPDATE] WORK HARD TRAVEL HARD APP

---
+++ app/(tabs)/explore.tsx
@@ -0,0 +1,45 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  Pressable,
+} from "react-native";
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>      
+      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <Text style={styles.btnText}>Work</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.btnText}>Travel</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,    
+    paddingHorizontal: 20,
+  },
+  header: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 100,
+  },
+  btnText: {
+    fontSize: 38,
+    fontWeight: "600",
+    color: "white",
+  },
+});
